b164ec6fae6ea9a6734ac58ddd8c3b89f73713fe
Curtis Maloney <curtis@commoncode.com.au>
2014-07-24 14:33:57 +1000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(
+    name='django-classy-settings',
+    version='0.1',
+    description='Simple class-based settings for Django',
+    author='Curtis Maloney',
+    author_email='curtis@tinbrain.net',
+    packages=['cbs',],
+)
