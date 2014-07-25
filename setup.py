539a6a0f15df985cfb322171756e33a340f8ea12
Curtis Maloney <curtis@commoncode.com.au>
2014-07-25 16:53:46 +1000

    Bumped version to 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='django-classy-settings',
-    version='0.1.0',
+    version='0.2.0',
     description='Simple class-based settings for Django',
     author='Curtis Maloney',
     author_email='curtis@tinbrain.net',
