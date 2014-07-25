9890dd38cdb9d23d970d34ec5b1d5452ca374039
Curtis Maloney <curtis@commoncode.com.au>
2014-07-25 10:16:11 +1000

    Follow conventional version format

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='django-classy-settings',
-    version='0.1',
+    version='0.1.0',
     description='Simple class-based settings for Django',
     author='Curtis Maloney',
     author_email='curtis@tinbrain.net',
