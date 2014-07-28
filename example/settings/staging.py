efc2761c932ce5723b09c89e18bce66138780f7f
Curtis Maloney <curtis@commoncode.com.au>
2014-07-28 12:36:46 +1000

    Update examples

--- example/settings/staging.py
+++ example/settings/staging.py
@@ -8,10 +8,8 @@ class StagingSettings(BaseSettings):
 
     DOCROOT = '/path/to/docroot/'
 
-    @property
     def STATIC_ROOT(self):
         return os.path.join(self.DOCROOT, 'static', '')
 
-    @property
     def MEDIA_ROOT(self):
         return os.path.join(self.DOCROOT, 'media', '')
