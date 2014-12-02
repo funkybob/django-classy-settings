2d1a7d5686c3af10bb7e877cc50185583e364180
funkybob <curtis@tinbrain.net>
2014-12-03 09:13:26 +1100

    Add different settings for Django versions

--- cbs/__init__.py
+++ cbs/__init__.py
@@ -97,14 +97,10 @@ def apply(name, to):
     })
 
 
-class BaseSettings(object):
+class CoreSettings(object):
     '''
-    A standard Django 1.6 setting
-
-    You must sub-class this and define PROJECT_NAME
+    Settings not likely to change between versions
     '''
-
-    DEBUG = True
 
     @property
     def BASE_DIR(self):
@@ -114,11 +110,29 @@ class BaseSettings(object):
             )
         )
 
+    DEBUG = True
+
     @property
-    def TEMPLATE_DEBUG(self):
+    def DEBUG_TEMPLATE(self):
         return self.DEBUG
 
     ALLOWED_HOSTS = []
+
+    @property
+    def ROOT_URLCONF(self):
+        return '{}.urls'.format(self.PROJECT_NAME)
+
+    @property
+    def WSGI_APPLICATION(self):
+        return '{}.wsgi.application'.format(self.PROJECT_NAME)
+
+
+class Base16Settings(CoreSettings):
+    '''
+    A standard Django 1.6 setting
+
+    You must sub-class this and define PROJECT_NAME
+    '''
 
     INSTALLED_APPS = (
         'django.contrib.admin',
@@ -137,14 +151,6 @@ class BaseSettings(object):
         'django.contrib.messages.middleware.MessageMiddleware',
         'django.middleware.clickjacking.XFrameOptionsMiddleware',
     )
-
-    @property
-    def ROOT_URLCONF(self):
-        return '{}.urls'.format(self.PROJECT_NAME)
-
-    @property
-    def WSGI_APPLICATION(self):
-        return '{}.wsgi.application'.format(self.PROJECT_NAME)
 
     # Database
     # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
@@ -175,3 +181,66 @@ class BaseSettings(object):
     # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
     STATIC_URL = '/static/'
+
+
+class Base17Settings(CoreSettings):
+
+    # Application definition
+
+    INSTALLED_APPS = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+    )
+
+    MIDDLEWARE_CLASSES = (
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    )
+
+    # Database
+    # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+    @property
+    def DATABASES(self):
+        return {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': os.path.join(self.BASE_DIR, 'db.sqlite3'),
+            }
+        }
+
+    # Internationalization
+    # https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+    LANGUAGE_CODE = 'en-us'
+
+    TIME_ZONE = 'UTC'
+
+    USE_I18N = True
+
+    USE_L10N = True
+
+    USE_TZ = True
+
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+    STATIC_URL = '/static/'
+
+
+from django import VERSION
+
+BASE_MAP = {
+    (1, 6): Base16Settings,
+    (1, 7): Base17Settings,
+}
+BaseSettings = BASE_MAP.get(VERSION[:2], Base16Settings)
