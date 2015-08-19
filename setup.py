df3bd0b155f9f2c611a1dcf17830934703a8cef7
Curtis Maloney <curtis@tinbrain.net>
2015-08-19 15:23:23 +1000

    Bumped version to 1.1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='django-classy-settings',
-    version='1.1.2',
+    version='1.1.3',
     description='Simple class-based settings for Django',
     author='Curtis Maloney',
     author_email='curtis@tinbrain.net',
