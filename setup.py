e9ce196be43f18933ad8aad7427ef192d6830026
Curtis Maloney <curtis@tinbrain.net>
2016-04-20 12:46:02 +1000

    Bump version to 2.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-classy-settings',
-    version='2.0.0',
+    version='2.0.1',
     description='Simple class-based settings for Django',
     author='Curtis Maloney',
     author_email='curtis@tinbrain.net',
